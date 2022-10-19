b83fcb4d2d7c84dc1e761a6f0f6fee8190bf445e
bds01088 <bds01088@naver.com>
2022-10-19 11:22:23 +0900

    [BE] ADD : 아이템, 풀깨비 Entity 추가 entity 추가 및 중계 테이블 생성 #hyo

---
+++ BackEnd/Dokcho2/src/main/java/com/ssafy/dokcho2/domain/item/Item.java
@@ -0,0 +1,38 @@
+package com.ssafy.dokcho2.domain.item;
+
+import com.ssafy.dokcho2.domain.userItem.UserItem;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Builder
+@AllArgsConstructor
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long itemId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "type")
+    private Integer type;
+
+    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL)
+    private List<UserItem> userItemList = new ArrayList<>();
+
+}
