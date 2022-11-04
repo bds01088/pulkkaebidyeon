1b63db24550783228eabbc783d005efe87908888
오하민 <ohm55545@naver.com>
2022-11-04 11:14:53 +0900

    [FE] FIX : MapChange
    Former-commit-id: 965faa7faebd5e24795223439ec24c54c33de278

---
+++ FrontEnd/dokcho2/src/components/modules/Player.js
@@ -0,0 +1,54 @@
+import { Body, Box, Vec3 } from 'cannon-es'
+
+export class Player {
+  constructor(info) {
+    this.moving = false
+    this.scene = info.scene
+    this.cannonWorld = info.cannonWorld
+    this.rotationY = info.rotationY || 0
+    console.log('aaaa', info)
+    this.width = info.width || 0.8
+    this.height = info.height || 0.5
+    this.depth = info.depth || 1
+    this.x = 0
+    this.y = 0
+    this.z = 0
+
+    info.gltfLoader.load(info.modelSrc, (glb) => {
+      glb.scene.traverse((child) => {
+        if (child.isMesh) {
+          child.castShadow = true
+        }
+      })
+
+      this.modelMesh = glb.scene
+      this.modelMesh.scale.x = 0.3
+      this.modelMesh.scale.y = 0.3
+      this.modelMesh.scale.z = 0.3
+      this.modelMesh.position.set(this.x, this.y, this.z)
+      this.modelMesh.name = 'ilbuni'
+      this.scene.add(this.modelMesh)
+
+      this.setCannonBody()
+    })
+  }
+  setCannonBody() {
+    const shape = new Box(
+      new Vec3(this.width / 2, this.height / 2, this.depth / 2)
+    )
+    this.cannonBody = new Body({
+      mass: 1,
+      position: new Vec3(this.x, this.y, this.z),
+      shape
+    })
+
+    this.cannonBody.quaternion.setFromAxisAngle(
+      new Vec3(0, 1, 0), // y축
+      this.rotationY
+    )
+
+    this.modelMesh.cannonBody = this.cannonBody
+
+    this.cannonWorld.addBody(this.cannonBody)
+  }
+}
