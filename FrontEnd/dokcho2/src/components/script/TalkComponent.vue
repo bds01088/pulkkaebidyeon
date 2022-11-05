9098bdb861380d18589c090941046b384b9c8417
오하민 <ohm55545@naver.com>
2022-11-05 22:03:42 +0900

    [FE] ADD talk component
    Former-commit-id: 84020ff05a5893a3a39f6cf079818f1e22c5cfd6

--- FrontEnd/dokcho2/src/components/script/TalkComponent.vue
+++ FrontEnd/dokcho2/src/components/script/TalkComponent.vue
@@ -1,19 +1,91 @@
 <template>
   <div class="talk">
-    대화창!
-
-    <button @click="$emit('talkClose')">닫기</button>
+    <div class="talk__box">
+      <div class="box" v-if="talk.content.line">
+        <div class="name">{{ this.name }}</div>
+        <div class="content">
+          {{ talk.content.line[talk.nowPage] }}
+        </div>
+        <button
+          @click="nextTalk()"
+          v-if="talk.content.line.length !== talk.nowPage + 1"
+        >
+          다음
+        </button>
+        <button @click="endTalk()" v-else>나가기</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import { BASE_URL } from '@/constant/BASE_URL'
+import { onMounted } from '@vue/runtime-core'
+import { ref } from 'vue'
+
+export default {
+  props: {
+    name: String
+  },
+  setup(props, { emit }) {
+    let talk = ref({ content: {}, nowPage: 0 })
+
+    function getLine() {
+      axios({
+        url: BASE_URL + '/api/v1/mission/' + props.name,
+        method: 'GET',
+        headers: {
+          AUTHORIZATION: 'Bearer ' + localStorage.getItem('accessToken')
+        }
+      })
+        .then((res) => {
+          console.log(res.data)
+          talk.value.content = res.data
+          talk.value.content.line = res.data.line.split('\\t')
+        })
+        .catch((err) => console.log(err))
+    }
+
+    function endTalk() {
+      const content = talk.value.content
+      const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+      console.log(content, userInfo)
+      if (userInfo.nowMissionId === content.missionId) {
+        axios({
+          url:
+            BASE_URL +
+            '/api/v1/mission/' +
+            userInfo.nowMissionId +
+            '?nowStatus=' +
+            content.status,
+          method: 'PUT',
+          headers: {
+            AUTHORIZATION: 'Bearer ' + localStorage.getItem('accessToken')
+          }
+        }).then(() => emit('talkClose'))
+      }
+    }
+
+    function nextTalk() {
+      talk.value.nowPage += 1
+    }
+
+    onMounted(() => getLine())
+
+    return {
+      talk,
+      endTalk,
+      nextTalk
+    }
+  }
+}
 </script>
 
 <style scoped>
 .talk {
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   justify-content: center;
   position: fixed;
   z-index: 30;
@@ -21,6 +93,18 @@ export default {}
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+}
+.talk__box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50%;
+}
+.box {
+  width: 80%;
+  height: 70%;
+  background-color: rgb(167, 105, 105);
 }
 </style>
