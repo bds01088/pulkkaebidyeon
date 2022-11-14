9c26d714bd617e37d3f9ce47ab49855d5e919f76
오하민 <ohm55545@naver.com>
2022-11-14 17:44:11 +0900

    [FE] FIX bootstrap
    Former-commit-id: b68824b0f66c012ab5f89a7d133c3e10cfe4e78f

--- FrontEnd/dokcho2/src/main.js
+++ FrontEnd/dokcho2/src/main.js
@@ -5,15 +5,11 @@ import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 library.add(fas)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
-  .use(BootstrapVue3)
   .use(store)
   .use(router)
   .mount('#app')
