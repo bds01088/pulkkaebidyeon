e9485fe9eb9788227a10abce9003c4c2443e62bc
오하민 <ohm55545@naver.com>
2022-11-17 17:26:47 +0900

    [FE] FIX battle end mymon position
    Former-commit-id: aea13fa6d0522efe97d2ec839bf235d4e1b25bca

--- FrontEnd/dokcho2/src/views/canvas/CanvasView.vue
+++ FrontEnd/dokcho2/src/views/canvas/CanvasView.vue
@@ -21,7 +21,7 @@
       :isloading="this.isloading"
     />
     <WorldCanvas
-      v-show="this.nowPage === 0"
+      v-show="this.nowPage === 0 && this.isBattle === 0"
       @changeCanvas="changeCanvas"
       @changeBattle="changeBattle"
       @talkStart="talkStart"
@@ -37,7 +37,7 @@
       :nowNavbar="this.nowNavbar"
     />
     <BattleCanvas
-      v-show="this.nowPage === 3"
+      v-show="this.isBattle === 1"
       @changeBattle="changeBattle"
       @startBattle="startBattle"
       :nowPage="this.nowPage"
@@ -59,6 +59,7 @@ export default {
   data() {
     return {
       nowPage: 0,
+      isBattle: 0,
       nowNavbar: false,
       startSignal: 0,
       isloading: 0,
@@ -107,11 +108,11 @@ export default {
     },
 
     changeBattle() {
-      if (this.nowPage === 0) {
-        this.nowPage = 3
+      if (this.nowPage === 0 && this.isBattle === 0) {
+        this.isBattle = 1
         this.startBattle()
       } else {
-        this.nowPage = 0
+        this.isBattle = 0
         this.audio.play()
       }
     },
